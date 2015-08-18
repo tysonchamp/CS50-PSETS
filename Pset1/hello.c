134b17eb20da4ea52c23214fc16118d3de0ab294
Soumya Mondal <tysonchamp@users.noreply.github.com>
2015-08-18 10:50:09 +0530

    Create hello.c

---
+++ Pset1/hello.c
@@ -0,0 +1,17 @@
+/**
+ * hello.c
+ * 
+ * Soumya Mondal
+ * tyson.granger181@gmail.com
+ * 
+ * CS50 Pset 1
+ */
+
+
+# include <stdio.h>
+
+// This my first C Program
+int main(void)
+{
+    printf("hello, world\n");
+}
